d4be66cb265457a432f240125b9a421d2f6baf04
Faramarz <pitofsoran33@gmail.com>
2021-03-28 00:58:09 +0430

    Made SpotifyAPI class

--- query.py
+++ query.py
@@ -1,28 +1,30 @@
+import webbrowser
 from spotify import *
 
+
 query = input("Artist: ")
-spotify = Spotify()
-artist_id, artist_name = spotify.search_artist(query)
+spotify_token = SpotifyToken()
+response = SpotifyAPI.search_artist(query, spotify_token.get_access_token())
+artist_id, artist_name = response["artists"]["items"][0]["id"], response["artists"]["items"][0]["name"]
 print(f"Retrieving songs for {artist_name}")
-albums, singles = spotify.get_artist_albums(artist_id)
+
+response = SpotifyAPI.get_artist_albums(artist_id, spotify_token.get_access_token())
+albums = response["items"]
 song_number = 1
 albums_number = 1
-album_ids = {}
-song_ids = {}
-for album_id in albums:
-    songs = spotify.get_album_tracks(album_id)
-    album_ids[albums_number] = album_id
-    print(str(albums_number) + ") " + albums[album_id] + ":")
+album_links = {}
+song_links = {}
+
+for album in albums:
+    response = SpotifyAPI.get_album_tracks(album["id"], spotify_token.get_access_token())
+    songs = response["items"]
+    album_links[albums_number] = album["external_urls"]["spotify"]
+    print(f"{albums_number}) {album['name']}:")
     albums_number += 1
     for song in songs:
-        song_ids[song_number] = song["id"]
-        print("\t" + str(song_number) + " " + song["name"])
+        song_links[song_number] = song["external_urls"]["spotify"]
+        print(f"\t{song_number}- {song['name']}")
         song_number += 1
-
-if len(singles) != 0:
-    print("Singles:")
-    for song in singles:
-        print("\t" + str(song_number) + " " + song)
 
 print("""
 ------------------
@@ -30,27 +32,27 @@ Done :)
 Source: Spotify
 ------------------
 """)
+
 user_request = input("Album or Singles? (type s or a)\n")
 song_request_text = "Number of the song: "
 album_request_text = "Number of the album: "
-query_run = True
-while query_run:
+while True:
     if user_request == "a":
         wanted_album_num = int(input(album_request_text))
-        if wanted_album_num in album_ids.keys():
-            wanted_album_id = album_ids[wanted_album_num]
-            Spotify.play_album(wanted_album_id)
-            query_run = False
+        if album_links.get(wanted_album_num) is not None:
+            wanted_album_href = album_links[wanted_album_num]
+            webbrowser.open(url=wanted_album_href)
+            break
         else:
             album_request_text = "Album was not found, try again. Number of the song: "
             wanted_album_num = int(input(album_request_text))
             continue
     elif user_request == "s":
         wanted_song_num = int(input(song_request_text))
-        if wanted_song_num in song_ids.keys():
-            wanted_song_id = song_ids[wanted_song_num]
-            Spotify.play_single(wanted_song_id)
-            query_run = False
+        if song_links.get(wanted_song_num) is not None:
+            wanted_song_href = song_links[wanted_song_num]
+            webbrowser.open(url=wanted_song_href)
+            break
         else:
             song_request_text = "Song was not found, try again. Number of the song: "
             wanted_song_num = int(input(song_request_text))
@@ -58,18 +60,3 @@ while query_run:
     else:
         user_request = input("Please type a for albums or s for songs.\n")
         continue
-
-# if user_request == "a":
-#     album_request = " ".join(input("What album to play? ").title().split())
-#     for album_id, album_name in albums.items():
-#         if album_request == album_name:
-#             album_request_id = album_id
-#             spotify.play_album_single(album_request_id)
-# elif user_request == "s":
-#     single_request = " ".join(input("What single to play? ").title().split())
-#     for song in singles:
-#         x = song.split()
-#         single_id = x[-1]
-#         str_song = " ".join(x[:-2])
-#         if single_request == str_song:
-#             spotify.play_album_single(single_id)
--- spotify.py
+++ spotify.py
@@ -1,107 +1,78 @@
+import datetime
+
 import requests
 import base64
-import datetime
-import webbrowser
 
 CLIENT_ID = "2fa050e95ec846d0b60ecdfff3912f5d"
 CLIENT_SECRET = "d33fe5c4675849db940d6bcb2130925f"
 
-http_proxy = "http://fodev.org:8118"
-https_proxy = "http://fodev.org:8118"
-
-proxyDict = {
-              "http": http_proxy,
-              "https": https_proxy,
-            }
+proxies = {
+    "http": "http://fodev.org:8118",
+    "https": "http://fodev.org:8118",
+}
 
 
-class SpotifyEndpoints:
-    TOKEN = "https://accounts.spotify.com/api/token"
-    SEARCH = "https://api.spotify.com/v1/search"
-    ALBUM = "https://api.spotify.com/v1/artists/{artist_id}/albums"
-    ALBUM_TRACKS = "https://api.spotify.com/v1/albums/{id}/tracks"
-    PLAY_ALBUM_URL = "https://open.spotify.com/album/{album_id}"
-    PLAY_SINGLE_URL = "https://open.spotify.com/track/{single_id}"
+class SpotifyAPI:
+    ENDPOINTS = {
+        "TOKEN": "https://accounts.spotify.com/api/token",
+        "SEARCH": "https://api.spotify.com/v1/search",
+        "ALBUM": "https://api.spotify.com/v1/artists/{artist_id}/albums",
+        "ALBUM_TRACKS": "https://api.spotify.com/v1/albums/{id}/tracks",
+    }
 
-
-class Spotify:
-
-    def __init__(self):
-        self.access_token = None
-        self.token_expire_time = -1
-
-    def get_access_token(self):
-        if self.access_token is not None and datetime.datetime.now().second < self.token_expire_time:
-            return self.access_token
-        client_credentials = base64.b64encode(f"{CLIENT_ID}:{CLIENT_SECRET}".encode())
+    @staticmethod
+    def get_token(client_id=CLIENT_ID, client_secret=CLIENT_SECRET):
+        client_credentials = base64.b64encode(f"{client_id}:{client_secret}".encode())
         r = requests.post(
-            url=SpotifyEndpoints.TOKEN,
+            url=SpotifyAPI.ENDPOINTS["TOKEN"],
             data={"grant_type": "client_credentials"},
             headers={"Authorization": f"Basic {client_credentials.decode()}"},
-            proxies=proxyDict
+            proxies=proxies
         )
         valid_request = r.status_code in range(200, 299)
         if not valid_request:
             raise Exception(f"cannot get token. status code: {r.status_code}")
-        token_response_data = r.json()
-        self.access_token = token_response_data["access_token"]
-        self.token_expire_time = token_response_data["expires_in"] + datetime.datetime.now().second
-        return self.access_token
+        return r.json()
 
-    def search_artist(self, query):
+    @staticmethod
+    def search_artist(query, access_token):
         r = requests.get(
-            url=SpotifyEndpoints.SEARCH,
-            headers={"Authorization": f"Bearer {self.get_access_token()}"},
+            url=SpotifyAPI.ENDPOINTS["SEARCH"],
+            headers={"Authorization": f"Bearer {access_token}"},
             params={"q": query, "type": "artist"},
-            proxies=proxyDict
+            proxies=proxies
         )
-        data = r.json()
-        artist_id = data["artists"]["items"][0]["id"]
-        artist_name = data["artists"]["items"][0]["name"]
-        return artist_id, artist_name
+        return r.json()
 
-    def get_artist_albums(self, artist_id):
-        singles = []
+    @staticmethod
+    def get_artist_albums(artist_id, access_token):
         r = requests.get(
-            url=SpotifyEndpoints.ALBUM.format(artist_id=artist_id),
-            headers={"Authorization": f"Bearer {self.get_access_token()}"},
+            url=SpotifyAPI.ENDPOINTS["ALBUM"].format(artist_id=artist_id),
+            headers={"Authorization": f"Bearer {access_token}"},
             params={"include_groups": "album,single"},
-            proxies=proxyDict
+            proxies=proxies
         )
-        data = r.json()
-        album_ids_names = {}
-        albums = data["items"]
-        for album in albums:
-            if album["album_type"] == "album":
-                if album["name"] not in album_ids_names.values():
-                    album_ids_names[album["id"]] = album["name"]
-                else:
-                    continue
-            else:
-                singles.append(album["name"] + " " + album["release_date"].split("-")[0])
+        return r.json()
 
-        return album_ids_names, singles
-
-    def get_album_tracks(self, album_id):
+    @staticmethod
+    def get_album_tracks(album_id, access_token):
         r = requests.get(
-            url=SpotifyEndpoints.ALBUM_TRACKS.format(id=album_id),
-            headers={"Authorization": f"Bearer {self.get_access_token()}"},
+            url=SpotifyAPI.ENDPOINTS["ALBUM_TRACKS"].format(id=album_id),
+            headers={"Authorization": f"Bearer {access_token}"},
             params={"limit": 50},
-            proxies=proxyDict
+            proxies=proxies
         )
-        data = r.json()
-        tracks = data["items"]
-        track_count = 1
-        track_ids = {}
-        for track in tracks:
-            track_ids[track_count] = track["external_urls"]["spotify"]
-            track_count += 1
-        return tracks
+        return r.json()
 
-    @staticmethod
-    def play_album(album_id):
-        return webbrowser.open(url=SpotifyEndpoints.PLAY_ALBUM_URL.format(album_id=album_id))
 
-    @staticmethod
-    def play_single(single_id):
-        return webbrowser.open(url=SpotifyEndpoints.PLAY_SINGLE_URL.format(single_id=single_id))
+class SpotifyToken:
+    def __init__(self):
+        self.access_token = None
+        self.token_expiration_time = -1
+
+    def get_access_token(self):
+        if self.access_token is None or self.token_expiration_time < datetime.datetime.now().second:
+            response = SpotifyAPI.get_token()
+            self.access_token = response["access_token"]
+            self.token_expiration_time = response["expires_in"] + datetime.datetime.now().second
+        return self.access_token
